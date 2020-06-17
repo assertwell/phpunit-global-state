1f27b79dd5ab9a0d4882d989502f83cf192d99c2
Steve Grunwell <steve@stevegrunwell.com>
2020-06-17 01:08:05 -0400

    Fix line-length on latest global variable test

--- tests/GlobalVariablesTest.php
+++ tests/GlobalVariablesTest.php
@@ -41,7 +41,7 @@ class GlobalVariablesTest extends TestCase
      * @test
      * @testdox setGlobalVariable() should be able to handle global variables that are redefined multiple times
      */
-    public function setGlobalVariable_should_be_able_to_handle_global_variables_that_are_redefined_multiple_times(): void
+    public function setGlobalVariable_should_be_able_to_handle_globals_that_are_redefined_multiple_times(): void
     {
         $GLOBALS['setGlobalVariable'] = 'old value';
         $this->setGlobalVariable('setGlobalVariable', 'new value');
